a2039960439aec66048bb8e05f9c4b14d420df23
Nikita <remnovmikita@gmail.com>
2025-11-25 15:12:52 +0200

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,7 @@
 
 const mainForm = document.querySelector(".login-form");
 
+const btn = document.querySelector(".login-form button")
 mainForm.addEventListener("submit", succes);
 
 function succes(event){
@@ -10,8 +11,10 @@ function succes(event){
     const password = form.elements.password.value;
 
     if (email.trim() === "" || password.trim() === ""){
+        btn.style.background = "red";
         return alert('All form fields must be filled in');
     }
+        btn.style.background = "#4e75ff";
     console.log(`Email: ${email}, Password: ${password}`);
     form.reset();
 }
